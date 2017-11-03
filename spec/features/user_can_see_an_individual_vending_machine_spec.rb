45197e320a2f14f1cd4756a920137e1656884a41
Adrian Lara <adrianblara@gmail.com>
2017-11-03 10:35:12 -0600

    many to many relation created

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -4,9 +4,12 @@ feature 'When a user visits a vending machine show page' do
   scenario 'they see the location and a list of snacks of that machine' do
     owner = Owner.create(name: "Sam's Snacks")
     dons  = owner.machines.create(location: "Don's Mixed Drinks")
-    snack_1 = dons.snacks.create(name: "drink_1", price: 1.00)
-    snack_2 = dons.snacks.create(name: "drink_2", price: 2.00)
-    snack_3 = dons.snacks.create(name: "drink_3", price: 1.50)
+    snack_1 = Snack.create(name: "drink_1", price: 1.00)
+    snack_2 = Snack.create(name: "drink_2", price: 2.00)
+    snack_3 = Snack.create(name: "drink_3", price: 1.50)
+    MachineSnacks.create(machine: dons, snack: snack_1)
+    MachineSnacks.create(machine: dons, snack: snack_2)
+    MachineSnacks.create(machine: dons, snack: snack_3)
 
     visit machine_path(dons)
 
